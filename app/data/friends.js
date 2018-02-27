86c7de383cf0d81d852555f3b0482605257bca61
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-02-27 16:43:26 -0600

    Added object array for friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+var friendProfile = [
+    {
+        name: 'Duke Nukem',
+        imgURL: 'http://domustation.altervista.org/wp-content/uploads/Duke-Nukem-Land-of-the-Babes-M5.png',
+        q1: 5,
+        q2: 5,
+        q3: 5,
+        q4: 5,
+        q5: 5
+    }
+]
+
+module.exports = friendProfile;
