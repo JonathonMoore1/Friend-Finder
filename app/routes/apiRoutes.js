903521c76c764bfa295d4109bc11d6da151e7b8f
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-03-01 16:39:57 -0600

    Nothing spectacular

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,6 +3,7 @@ var friendData = require('../data/friends.js');
 module.exports = function(app) {
     app.get('/api/friends', function(req, res) {
         res.json(friendData);
+        console.log('Data sent successfully');
     });
-    console.log('Data sent successfully');
+   
 }
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -4,12 +4,12 @@ var path = require('path');
 // Export GET and POST requests
 module.exports = function(app) {
     app.get('/home', function(req, res) {
-        res.sendFile(path.join('../public/home.html'));
+        res.sendFile(path.join(__dirname, '../public/home.html'));
         console.log('Displaying homepage...');
     });
 
     app.get('/survey', function(req, res) {
-        res.sendFile(path.join('../public/survey.html'));
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
         console.log('Displaying survey');
     });
 }
