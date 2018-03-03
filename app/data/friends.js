0a5f09af96f1dcc527eca5baae5aae1c36137654
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-03-02 20:21:07 -0600

    Post requests working. Still have yet to do write matching logic and get modal working.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,11 +2,11 @@ var friendData = [
     {
         name: 'Duke Nukem',
         imgURL: 'http://domustation.altervista.org/wp-content/uploads/Duke-Nukem-Land-of-the-Babes-M5.png',
-        q1: 5,
-        q2: 5,
-        q3: 5,
-        q4: 5,
-        q5: 5
+        scores: [
+            5,
+            5,
+            5
+        ]
     }
 ]
 
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,7 +3,11 @@ var friendData = require('../data/friends.js');
 module.exports = function(app) {
     app.get('/api/friends', function(req, res) {
         res.json(friendData);
-        console.log('Data sent successfully');
+        console.log("Data sent successfully");
     });
-   
+
+    app.post('/api/friends', function(req, res) {
+        friendData.push(req.body);
+        console.log("New cuddle buddy added to data object");
+    });
 }
